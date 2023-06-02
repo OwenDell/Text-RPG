56d2294b1650bded772f2b2ef2b4eb1b33be95f7
PyroDellz <owendell444@gmail.com>
2023-06-02 13:22:29 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -15,3 +15,9 @@ def weighted_random(choices):
         if chance[1] >= random.randint(0, sum([possible_choices[i][1] for i in range(len(possible_choices))])):
             return chance[0]
         possible_choices.remove(chance)
+        
+def capitalize(string):
+    string = string.split()
+    for index, word in enumerate(string):
+        string[index] = word[0].upper() + word[1:].lower()
+    return string
